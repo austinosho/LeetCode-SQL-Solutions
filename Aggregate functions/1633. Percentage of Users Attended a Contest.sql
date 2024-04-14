bf078e6f3481dd0fe01e61de746f1fb025171a44
Augustine Oshodin <166131518+austinosho@users.noreply.github.com>
2024-04-14 22:13:23 +0000

    Update 1633. Percentage of Users Attended a Contest.sql

--- Aggregate functions/1633. Percentage of Users Attended a Contest.sql
+++ Aggregate functions/1633. Percentage of Users Attended a Contest.sql
@@ -1,1 +1,6 @@
-
+# Write your MySQL query statement below
+select r.contest_id, (ROUND((COUNT(DISTINCT r.user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users)) * 100, 2)) as percentage from Register as r
+cross join Users as u
+on r.user_id = u.user_id
+group by r.contest_id
+order by percentage desc, r.contest_id asc;
