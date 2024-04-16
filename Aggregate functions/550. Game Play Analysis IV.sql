431e46db884768a9bab5dda1f9f18ab586ca2f87
Augustine Oshodin <166131518+austinosho@users.noreply.github.com>
2024-04-16 22:31:10 +0000

    Update 550. Game Play Analysis IV.sql

--- Aggregate functions/550. Game Play Analysis IV.sql
+++ Aggregate functions/550. Game Play Analysis IV.sql
@@ -1,1 +1,4 @@
-
+# Write your MySQL query statement below
+SELECT ROUND(COUNT(a.player_id) / (SELECT COUNT(DISTINCT c.player_id) FROM Activity c), 2) AS fraction
+FROM Activity a WHERE (a.player_id, DATE_SUB(a.event_date, INTERVAL 1 DAY)) 
+IN (SELECT b.player_id, MIN(b.event_date) FROM Activity b GROUP BY b.player_id);
